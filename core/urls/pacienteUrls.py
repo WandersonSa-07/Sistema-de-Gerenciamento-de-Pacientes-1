dec393b9bb4331a3a8e17626c318108795e57b03
Wanderson Carlos <gabrielassantos27@gmail.com>
2024-11-26 21:01:32 -0300

    Tentativa de fazer com que a receita seja gerada em pdf

--- core/urls/pacienteUrls.py
+++ core/urls/pacienteUrls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from core.views.pacienteView import pacienteRegistro
+from core.views.pacienteView import pacienteRegistro, visualizar_receita
 
 urlpatterns = [
-    path("", pacienteRegistro, name='pacienteRegistro')
+    path("", pacienteRegistro, name='pacienteRegistro'),
+    path('receita/<int:receita_id>/pdf/', visualizar_receita, name='gerar_pdf'),
+
 ]
--- core/views/pacienteView.py
+++ core/views/pacienteView.py
@@ -1,6 +1,11 @@
 from pyexpat.errors import messages
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from core.forms.registroForm import PacienteForm
+from core.models import ReceitaMedica
+from weasyprint import HTML
+import weasyprint
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 def pacienteRegistro(request):
    if str(request.method) == 'POST':
@@ -21,3 +26,12 @@ def pacienteRegistro(request):
    }
 
    return render(request,'registro.html', context=context)
+
+def visualizar_receita(request, receita_id):
+   receita = get_object_or_404(ReceitaMedica, id=receita_id)
+   pdf_gerado = render_to_string("receita_pdf.html", {"receita":receita})
+   pdf = HTML(string = pdf_gerado).write_pdf()
+
+   response = HttpResponse(pdf, content_type='application/pdf')
+   response['Content-Disposition'] = f'attachment; filename="receita{receita.id}.pdf"'
+   return response
